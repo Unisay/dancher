79ea4586f02dc1d0c7545f1ac8709ec9577e1ad2
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-08-11 17:33:42 +0200

    Log topics to console

--- frontend/src/App/Events.purs
+++ frontend/src/App/Events.purs
@@ -1,25 +1,30 @@
 module App.Events where
 
+import Prelude
 import App.Routes (Route)
 import App.State (State(..))
-import Data.Function (($))
+import App.Types (Topic)
+import Control.Monad.Aff.Console (CONSOLE, log)
+import Data.Maybe (Maybe(..))
+import Data.String (joinWith)
 import Network.HTTP.Affjax (AJAX)
 import Pux (EffModel, noEffects)
-import App.Types (Topic)
 
 data Event = PageView Route
   | AppWaiting
   | AppStarted (Array Topic)
   | GetTopics
 
-type AppEffects fx = (ajax :: AJAX | fx)
+type AppEffects fx = (ajax :: AJAX, console :: CONSOLE | fx)
 
 foldp :: ∀ fx. Event -> State -> EffModel State Event (AppEffects fx)
 foldp (PageView route) (State st) =
   noEffects $ State st { route = route, loaded = true }
-foldp AppWaiting (State st) =
-  noEffects $ State st
-foldp (AppStarted topics) (State st) =
-  noEffects $ State st { topics = topics }
+foldp AppWaiting s =
+  noEffects s
+foldp (AppStarted topics) s@(State st) =
+  { state: State st { topics = topics }
+  , effects: [ (log $ joinWith "; " (map show topics)) *> pure Nothing ]
+  }
 foldp GetTopics  (State st) =
   noEffects $ State st
