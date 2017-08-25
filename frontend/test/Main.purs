98cc99d69324b62a80ca74966ec8f133f50cf50f
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-08-25 12:09:15 +0200

    Arbitrary instances

--- frontend/test/Main.purs
+++ frontend/test/Main.purs
@@ -9,32 +9,19 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson)
 import Data.Either (either)
 import Data.Newtype (class Newtype)
-import Test.QuickCheck (class Arbitrary, arbitrary)
 import Test.Unit (Test, failure, suite, test, timeout)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 import Test.Unit.QuickCheck (quickCheck)
+import Test.Arbitraries
 
 main = runTest do
   suite "Json roundtrip" do
     test "State" do
       quickCheck stateRoundtrip
 
-newtype TestState = TestState State
-derive newtype instance eqTestState :: Eq TestState
-derive newtype instance showTestState :: Show TestState
-derive newtype instance encodeTestState :: EncodeJson TestState
-derive newtype instance decodeTestState :: DecodeJson TestState
-
-instance arbitraryTestState :: Arbitrary TestState where
-  arbitrary = do
-    title <- arbitrary
-    route <- ?x
-    pure $ TestState $ State { title: title, route: route,  }
-
-
 roundTrip :: ∀ a e. Eq a => EncodeJson a => DecodeJson a => a -> Boolean
 roundTrip a = either (const false) (eq a) $ decodeJson (encodeJson a)
 
-stateRoundtrip :: ∀ e. TestState -> Boolean
+stateRoundtrip :: ∀ e. ArbState -> Boolean
 stateRoundtrip = roundTrip
