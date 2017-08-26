d986a881f5ed51685874ce09d98da51fa537e202
Yuriy Lazarev <unisay@gmail.com>
2017-08-26 20:47:55 +0200

    nodejs buildpack

--- frontend/bundle-full.sh
+++ frontend/bundle-full.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-pulp browserify \
+echo "Dancher: building a fully optimized bundle"
+
+pulp \
+  --monochrome \
+  browserify \
   --jobs 4 \
   --force \
   --optimise \
@@ -8,10 +12,8 @@ pulp browserify \
   | uglifyjs \
   --compress \
   --mangle \
-  --verbose \
   --timings \
-  --warn \
   -o ./static/bundle.js
 
-
-
+echo "Bundle is ready: "
+ls -lah ./static/bundle.js
